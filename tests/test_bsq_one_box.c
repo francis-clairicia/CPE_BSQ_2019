c933e411e9716dad70c11edb69a89ae14c80caab
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2019-11-14 12:02:25 +0100

    Change criterion tests

--- tests/test_bsq_one_box.c
+++ tests/test_bsq_one_box.c
@@ -22,3 +22,17 @@ Test(bsq, intermediate_map_one_empty_box)
     free_buffer(&buffer);
     free_buffer(&result);
 }
+
+Test(bsq, intermediate_map_one_filled_box)
+{
+    char map_test[] = "mouli_maps/"
+    "intermediate_map_one_filled_box";
+    char map_expected[] = "mouli_maps_solved/"
+    "intermediate_map_one_filled_box";
+    buffer_t *buffer = bsq(map_test);
+    buffer_t *result = read_buffer(map_expected);
+
+    cr_expect_str_eq(buffer->string, result->string);
+    free_buffer(&buffer);
+    free_buffer(&result);
+}
---
+++ tests/test_bsq_one_column.c
@@ -0,0 +1,80 @@
+/*
+** EPITECH PROJECT, 2019
+** Test bsq()
+** File description:
+** Test a function
+*/
+
+#include <criterion/criterion.h>
+#include <my.h>
+#include <bsq.h>
+
+Test(bsq, intermediate_map_one_empty_column)
+{
+    char map_test[] = "mouli_maps/"
+    "intermediate_map_one_empty_column";
+    char map_expected[] = "mouli_maps_solved/"
+    "intermediate_map_one_empty_column";
+    buffer_t *buffer = bsq(map_test);
+    buffer_t *result = read_buffer(map_expected);
+
+    cr_expect_str_eq(buffer->string, result->string);
+    free_buffer(&buffer);
+    free_buffer(&result);
+}
+
+Test(bsq, intermediate_map_one_filled_column)
+{
+    char map_test[] = "mouli_maps/"
+    "intermediate_map_one_filled_column";
+    char map_expected[] = "mouli_maps_solved/"
+    "intermediate_map_one_filled_column";
+    buffer_t *buffer = bsq(map_test);
+    buffer_t *result = read_buffer(map_expected);
+
+    cr_expect_str_eq(buffer->string, result->string);
+    free_buffer(&buffer);
+    free_buffer(&result);
+}
+
+Test(bsq, intermediate_map_one_column_with_obstacles_25pc)
+{
+    char map_test[] = "mouli_maps/"
+    "intermediate_map_one_column_with_obstacles_25pc";
+    char map_expected[] = "mouli_maps_solved/"
+    "intermediate_map_one_column_with_obstacles_25pc";
+    buffer_t *buffer = bsq(map_test);
+    buffer_t *result = read_buffer(map_expected);
+
+    cr_expect_str_eq(buffer->string, result->string);
+    free_buffer(&buffer);
+    free_buffer(&result);
+}
+
+Test(bsq, intermediate_map_one_column_with_obstacles_50pc)
+{
+    char map_test[] = "mouli_maps/"
+    "intermediate_map_one_column_with_obstacles_50pc";
+    char map_expected[] = "mouli_maps_solved/"
+    "intermediate_map_one_column_with_obstacles_50pc";
+    buffer_t *buffer = bsq(map_test);
+    buffer_t *result = read_buffer(map_expected);
+
+    cr_expect_str_eq(buffer->string, result->string);
+    free_buffer(&buffer);
+    free_buffer(&result);
+}
+
+Test(bsq, intermediate_map_one_column_with_obstacles_75pc)
+{
+    char map_test[] = "mouli_maps/"
+    "intermediate_map_one_column_with_obstacles_75pc";
+    char map_expected[] = "mouli_maps_solved/"
+    "intermediate_map_one_column_with_obstacles_75pc";
+    buffer_t *buffer = bsq(map_test);
+    buffer_t *result = read_buffer(map_expected);
+
+    cr_expect_str_eq(buffer->string, result->string);
+    free_buffer(&buffer);
+    free_buffer(&result);
+}
